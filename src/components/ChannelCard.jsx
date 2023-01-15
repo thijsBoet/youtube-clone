be1205286d5636d82a3edb84ae26935ec039b878
thijsBoet <m.boet@chello.nl>
2023-01-15 15:39:22 +0100

    added video and channel pages

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+
+import { demoProfilePicture } from '../utils/constants';
+
+const ChannelCard = ({ channelDetail }) => (
+	<Box
+		sx={{
+			boxShadow: 'none',
+			borderRadius: '20px',
+			display: 'flex',
+			justifyContent: 'center',
+			alighItems: 'center',
+			width: { md: '320px', xs: '356px' },
+			height: { md: '320px', xs: '356px' },
+			margin: 'auto',
+		}}
+	>
+		<Link to={`/channel/${channelDetail?.id?.channelId}`}>
+			<CardContent
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					alignItems: 'center',
+					color: '#fff',
+				}}
+			>
+				<CardMedia
+					image={
+						channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture
+					}
+					alt={channelDetail?.snippet?.title}
+					sx={{
+						width: '180px',
+						height: '180px',
+						borderRadius: '50%',
+						border: '1px solid #e3e3e3',
+					}}
+				/>
+				<Typography variant='h6'>
+					{channelDetail?.snippet?.title}
+					<CheckCircle sx={{ fontSize: '14px', color: 'gray', ml: '5px' }} />
+				</Typography>
+				{channelDetail?.statistics?.subscriberCount && (
+					<Typography variant='subtitle2'>
+						{parseInt(
+							channelDetail?.statistics?.subscriberCount
+						).toLocaleString()}
+						Subscribers
+					</Typography>
+				)}
+			</CardContent>
+		</Link>
+	</Box>
+);
+
+export default ChannelCard;
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+	const [channelDetail, setChannelDetail] = useState();
+	const [videos, setVideos] = useState(null);
 
-export default ChannelDetail
+	const { id } = useParams();
+
+	useEffect(() => {
+		const fetchResults = async () => {
+			const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
+
+			setChannelDetail(data?.items[0]);
+
+			const videosData = await fetchFromAPI(
+				`search?channelId=${id}&part=snippet%2Cid&order=date`
+			);
+
+			setVideos(videosData?.items);
+		};
+
+		fetchResults();
+	}, [id]);
+	return (
+		<Box>
+			<ChannelCard channelDetail={channelDetail} marginTop='-93px' />
+		</Box>
+	);
+};
+
+export default ChannelDetail;
