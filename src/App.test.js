c2b27129fa689f938bc40b876a32f76b28cb9c48
thijsBoet <m.boet@chello.nl>
2023-01-15 02:08:57 +0100

    implemented sidebar and searchbar UI

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('should display a logo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const logo = screen.getByAltText('logo');
+  expect(logo).toBeInTheDocument();
 });
